a4068bbaa7f83201009f1d30a86da4e21491c1bc
albothyl <jjhwqqq@naver.com>
2014-06-25 15:56:38 +0900

    1. customAnnotation move project to common

--- market/src/main/java/org/project/common/system/customAnnotation/Util.java
+++ market/src/main/java/org/project/common/system/customAnnotation/Util.java
@@ -1,4 +1,4 @@
-package org.project.system.customAnnotation;
+package org.project.common.system.customAnnotation;
 
 import java.lang.annotation.Documented;
 import java.lang.annotation.ElementType;
